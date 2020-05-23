b83b55e1dca8d84b2825e10df40399dda5c43996
Mukesh0086 <mukeshgsmukesh@gmail.com>
2020-05-23 12:53:01 +0530

    [Mukesh] add.  GetBooks Component added

---
+++ src/Components/GetBook.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Paper, Button, Popover } from "@material-ui/core";
+
+class GetBook extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { paperElevation: 1, item: this.props.item, descOpen: false };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState = { item: this.props.item };
+  }
+
+  render() {
+    return (
+      <div>
+        <Paper elevation={1} id="paper-book">
+          <Paper elevation={1} id="paper-book-below">
+            <div className="book-img-div">
+              <img src=/*{this.state.item.bookImage}*/"../Assets/book1.svg"
+               className="book-image" />
+            </div>
+          </Paper>
+          <div className="outer-details-div">
+            <div className="div-bookname">{this.state.item.bookName}</div>
+            <div className="div-author">by {this.state.item.bookAuthor}</div>
+            <div className="div-price">Rs. {this.state.item.bookPrice}</div>
+            <div className="div-buttons">
+              <Button id="div-bagbutton">Add To Bag</Button>
+              <Button id="div-wishlistbutton" variant="outlined">
+                Wishlist
+              </Button>
+            </div>
+          </div>
+        </Paper>
+        <Popover open={this.state.descOpen}></Popover>
+      </div>
+    );
+  }
+}
+
+export default GetBook;
