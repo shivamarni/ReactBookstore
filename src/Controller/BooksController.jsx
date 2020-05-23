46979cce5f7e7417c67ed4ba59bef828c67ace25
Mukesh0086 <mukeshgsmukesh@gmail.com>
2020-05-23 12:53:52 +0530

    [Mukesh] add. BooksController added

---
+++ src/Controller/BooksController.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+//require("dotenv").config();
+
+var controller = {
+  getallbooks(pageNo) {
+    return axios.get(
+      //   process.env.REACT_APP_BASE_URL + `/book/displaybooks/${page}`
+      "http://localhost:8085/pagination/booksbyid/",
+      {
+        headers: {
+          pageNo: pageNo,
+          "Content-type": "application/json ",
+        },
+      }
+    );
+  },
+  getcountofbooks() {
+    return axios.get("http://localhost:8085/book/getbookcount");
+  },
+  getbooksPriceDesc(pageNo) {
+    return axios.get(
+      `http://localhost:8085/pagination/booksbyprice/descending/`,
+      {
+        headers: {
+          pageNo: pageNo,
+          "Content-type": "application/json ",
+        },
+      }
+    );
+  },
+  getbooksPriceAsc(pageNo) {
+    return axios.get(
+      `http://localhost:8085/pagination/booksbyprice/ascending/`,
+      {
+        headers: {
+          pageNo: pageNo,
+          "Content-type": "application/json ",
+        },
+      }
+    );
+  },
+  getbooksNewest(pageNo) {
+    return axios.get(
+      `http://localhost:8085/pagination/booksbyprice/ascending/`,
+      {
+        headers: {
+          pageNo: pageNo,
+          "Content-type": "application/json ",
+        },
+      }
+    );
+  },
+};
+
+export default controller;
